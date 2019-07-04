2e39b5636734b7282cd3c12a1b004c2b54312c67
강병우 <kbw2204@gmail.com>
2019-07-05 03:02:04 +0900

    edit view

--- snapkitTest/snapkitTest/Controller/ViewController.swift
+++ snapkitTest/snapkitTest/Controller/ViewController.swift
@@ -10,21 +10,56 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    
-
+    lazy var view1: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.red
+        return view
+    }()
+    lazy var view2: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.blue
+        return view
+    }()
+    lazy var view3: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.black
+        return view
+    }()
+    lazy var view4: UIView = {
+        var view = UIView()
+        view.backgroundColor = UIColor.black
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let box = UIView()
-        superview.addSubview(box)
-        
-        box.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(superview).offset(20)
-            make.left.equalTo(superview).offset(20)
-            make.bottom.equalTo(superview).offset(-20)
-            make.right.equalTo(superview).offset(-20)
+        self.view.addSubview(view1)
+        view1.addSubview(view2)
+        view1.addSubview(view3)
+        view1.snp.makeConstraints { (a) in
+            a.width.equalTo(view)
+            a.height.equalTo(60)
+            a.top.equalTo(44)
         }
-        
+        view2.snp.makeConstraints { (a) in
+            a.width.equalTo(view1.snp.height)
+            a.height.equalTo(view1.snp.height)
+            a.left.equalTo(view1)
+            a.top.equalTo(view1)
+        }
+        view3.snp.makeConstraints { (a) in
+            a.width.equalTo(view1.snp.height)
+            a.height.equalTo(view1.snp.height)
+            a.right.equalTo(view1)
+            a.top.equalTo(view1)
+        }
+        view.addSubview(view4)
+        view4.snp.makeConstraints { (a) in
+            a.width.equalTo(view).dividedBy(5)
+            a.height.equalTo(view.snp.width).dividedBy(5)
+            a.bottom.equalTo(view).offset(-44)
+            a.left.equalTo(view)
+        }
     }
 
 
